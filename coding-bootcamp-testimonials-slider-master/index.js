3b9350b80c123e3898248b7b613e9510ff1e83f8
Mesut Çifci <mesutcifci97@gmail.com>
2020-12-22 23:20:09 +0300

    coding bootcamp testimonials slider updated
    -some js added
    -some css effect added

---
+++ coding-bootcamp-testimonials-slider-master/index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+let tanya = document.getElementById("page-tanya");
+let john = document.getElementById("page-john");
+
+/*
+for (let i = 0; i < 2; i++) {
+    buttons[i].addEventListener("click", () => {
+        tanya.classList.toggle("hide");
+        john.classList.toggle("hide");
+        console.log("clicked");
+    });
+}
+*/
+
+buttons.forEach(
+    addEventListener("click", () => {
+        tanya.classList.toggle("hide");
+        john.classList.toggle("hide");
+        console.log("clicked");
+    })
+);
