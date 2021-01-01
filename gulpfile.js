7757a2cadabe65b228616a2edcc3654effab371f
Mesut Çifci <mesutcifci97@gmail.com>
2021-01-01 15:30:04 +0300

    gulpfile.js updated package.json updated
    -sass and autoprefixer tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
 
+let browserList = [
+    'last 2 versions'
+];
+
+gulp.task('four-card', () => {
+    return gulp.src("./four-card-feature-section-master/scss/**/*.scss")
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(prefix({ overrideBrowserslist: browserList, cascade: false  }))
+    .pipe(gulp.dest("./four-card-feature-section-master/css"))
+});
+
+gulp.task('base-apparel', () => {
+    return gulp.src("./base-apparel-coming-soon-master/scss/**/*.scss")
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(prefix({ overrideBrowserslist: browserList, cascade: false  }))
+    .pipe(gulp.dest("./base-apparel-coming-soon-master/css"))
+});
+
+gulp.task('watch', () => {
+    gulp.watch("./four-card-feature-section-master/scss/**/*.scss", gulp.series("four-card"));
+    gulp.watch("./base-apparel-coming-soon-master/scss/**/*.scss", gulp.series("base-apparel"));
+});
