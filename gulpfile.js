a21e9c91a08dc775cf5a6ed26b47a8700eb0b260
Mesut Çifci <mesutcifci97@gmail.com>
2021-02-05 03:29:39 +0300

    fylo dark theme landing page initial commit
    mobile design almost finished

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,18 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
 
-let browserList = [
-    'last 2 versions'
-];
 
 gulp.task('four-card', () => {
     return gulp.src("./four-card-feature-section-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./four-card-feature-section-master/css"))
 });
 
 gulp.task('base-apparel', () => {
     return gulp.src("./base-apparel-coming-soon-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./base-apparel-coming-soon-master/css"))
 });
 
@@ -24,21 +21,21 @@ gulp.task('base-apparel', () => {
 gulp.task('intro-component', () => {
     return gulp.src("./intro-component-with-signup-form-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./intro-component-with-signup-form-master/css"))
 });
 
 gulp.task('single-price', () => {
     return gulp.src("./single-price-grid-component-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./single-price-grid-component-master/css"))
 });
 
 gulp.task('ping-coming-soon', () => {
     return gulp.src("./ping-coming-soon-page-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./ping-coming-soon-page-master/css"))
 });
 
@@ -46,7 +43,7 @@ gulp.task('ping-coming-soon', () => {
 gulp.task('huddle-landing-page', () => {
     return gulp.src("./huddle-landing-page-with-single-introductory-section-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./huddle-landing-page-with-single-introductory-section-master/css"))
 });
 
@@ -54,8 +51,15 @@ gulp.task('huddle-landing-page', () => {
 gulp.task('project-tracking-intro', () => {
     return gulp.src("./project-tracking-intro-component-master/scss/**/*.scss")
         .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(prefix({ overrideBrowserslist: browserList, cascade: false }))
+        .pipe(prefix({ cascade: false }))
         .pipe(gulp.dest("./project-tracking-intro-component-master/css"))
+});
+
+gulp.task('fylo-dark-theme-landing', () => {
+    return gulp.src("./fylo-dark-theme-landing-page-master/scss/**/*.scss")
+    .pipe(sass({ outputStyle: 'expanded'}))
+    .pipe(prefix( {cascade: false} ))
+    .pipe(gulp.dest('./fylo-dark-theme-landing-page-master/css'))
 });
 
 gulp.task('watch', () => {
@@ -66,4 +70,5 @@ gulp.task('watch', () => {
     gulp.watch("./ping-coming-soon-page-master/scss/**/*.scss", gulp.series("ping-coming-soon"));
     gulp.watch("./huddle-landing-page-with-single-introductory-section-master/scss/**/*.scss", gulp.series("huddle-landing-page"));
     gulp.watch("./project-tracking-intro-component-master/scss/**/*.scss", gulp.series("project-tracking-intro"));
+    gulp.watch("./fylo-dark-theme-landing-page-master/scss/**/*.scss", gulp.series('fylo-dark-theme-landing'))
 });
