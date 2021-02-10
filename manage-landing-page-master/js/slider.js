b437da5ee10fbe6b6ad70acf49cf3517a47c799e
Mesut Çifci <mesutcifci97@gmail.com>
2021-02-10 16:14:01 +0300

    mobile design almost finished

--- manage-landing-page-master/js/slider.js
+++ manage-landing-page-master/js/slider.js
@@ -1,16 +1,53 @@
 let cards = document.getElementsByClassName("card");
-let dots  = document.getElementsByClassName("dot");
+let dots = document.getElementsByClassName("dot");
 
 cards = Array.from(cards);
-dots  = Array.from(dots);
+dots = Array.from(dots);
 
 
-function translateCard(elementIndex) {
-  cards[elementIndex].classList.toggle("js-translate");
+function removeClass(array, className, elementIndex) {
+
+  array.forEach((element, currentIndex, array) => {
+    if (elementIndex != currentIndex) {
+      element.classList.remove(className);
+    }
+  });
+
+}
+
+function slideCard(elementIndex) {
+
+  removeClass(cards, "card--default", elementIndex);
+
+  for (let i = 0; i < elementIndex; i++) {
+
+    if (cards[i].classList.contains("card--next")) {
+      cards[i].classList.remove("card--next");
+      cards[i].classList.add("card--prev");
+    }
+
+  }
+
+  for (let i = elementIndex; i < cards.length; i++) {
+    if (cards[i].classList.contains("card--prev")) {
+      cards[i].classList.remove("card--prev");
+      cards[i].classList.add("card--next");
+    }
+  }
+
+  cards[elementIndex].classList.add("card--default");
+}
+
+function markDot(elementIndex) {
+  if (dots[elementIndex].classList.contains("dot--selected") == false) {
+    dots[elementIndex].classList.add("dot--selected");
+    removeClass(dots,"dot--selected", elementIndex);
+  }
 }
 
 dots.forEach((element, elementIndex, array) => {
-   element.addEventListener("click", () => {
-     translateCard(elementIndex);
-   }) 
+  element.addEventListener("click", () => {
+    slideCard(elementIndex);
+    markDot(elementIndex);
+  })
 });
